27c6121ac30754a2f6339beb0db0d0e0c06a0ef9
Angelo-Ponce <angeloponcedev@gmail.com>
2025-03-16 22:12:03 -0500

    add CircuitBreaker

--- transaction-root/src/main/java/com/devsu/service/impl/ClientService.java
+++ transaction-root/src/main/java/com/devsu/service/impl/ClientService.java
@@ -1,18 +1,27 @@
 package com.devsu.service.impl;
 
 import com.devsu.dto.ClientDTO;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ClientService {
 
     private final RestTemplate restTemplate;
 
+    @CircuitBreaker(name = "clientService", fallbackMethod = "fallbackFindClientById")
     public ClientDTO findClientById(String clientId) {
         String url = "http://localhost:8080/api/v1/clientes/clientId/" + clientId;
         return restTemplate.getForObject(url, ClientDTO.class);
+    }
+
+    private ClientDTO fallbackFindClientById(String clientId, Throwable ex) {
+        log.error("Error al obtener client {}. Causa: {}", clientId, ex.getMessage());
+        return null;
     }
 }
